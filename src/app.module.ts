4addef7ef0a0632a6630d7d041be0e3b01872f8e
Mykola Shymechko <m.shymechko@brainence.com>
2022-04-20 13:05:57 +0300

    base CRUD track

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
 import { TrackModule } from "./track/track.module";
 
 
 
 @Module({
-  imports: [TrackModule]
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://shema:vbrjkf1@cluster0.qqhgt.mongodb.net/test-aplication?retryWrites=true&w=majority'),
+    TrackModule
+  ]
 })
 export class AppModule {
 
--- src/track/track.controller.ts
+++ src/track/track.controller.ts
@@ -1,23 +1,38 @@
-import { Controller, Get } from "@nestjs/common"; 
-
+import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common"; 
+import { CreateTrackDto } from "./dto/create-track.dto";
+import { TrackService } from "./track.service";
+import {ObjectId} from 'mongoose'
+import { CreateCommentDto } from "./dto/create-comment.tdo";
 
 @Controller('/tracks')
 export class TrackController {
-  
-   create() {
-    
+
+  constructor(
+    private trackService: TrackService
+  ){}
+
+  @Post()
+   create(@Body() dto: CreateTrackDto) {
+    return this.trackService.create(dto)
   }
 
   @Get()
    getAll() {
-    return "work"
+    return this.trackService.getAll()
   }
 
-   getOne() {
-
+  @Get(':id')
+  getOne(@Param('id') id: ObjectId) {
+    return this.trackService.getOne(id)
   }
 
-   delete() {
-
+  @Delete(':id')
+   delete(@Param('id') id: ObjectId) {
+    return this.trackService.delete(id)
   }
+
+  @Post('/comment')
+  addComent(@Body() dto: CreateCommentDto) {
+   return this.trackService.addComment(dto)
+ }
 }
